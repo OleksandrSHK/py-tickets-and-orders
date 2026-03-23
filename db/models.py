be3546d8943926b1f8eed9838745622f840342b5
Oleksandr Shkliar <shklyara29@gmail.com>
2026-03-23 13:11:41 +0200

    Fixed __str__ in Order, Ticket

--- db/models.py
+++ db/models.py
@@ -74,7 +74,7 @@ class Order(models.Model):
         ordering = ["-created_at"]
 
     def __str__(self) -> str:
-        return f"<Order: {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}>"
+        return self.created_at.strftime("%Y-%m-%d %H:%M:%S")
 
 
 class Ticket(models.Model):
@@ -96,9 +96,9 @@ class Ticket(models.Model):
         ]
 
     def __str__(self) -> str:
-        return (f"<Ticket: {self.movie_session.movie.title} "
+        return (f"{self.movie_session.movie.title} "
                 f"{self.movie_session.show_time.strftime('%Y-%m-%d %H:%M:%S')}"
-                f" (row: {self.row}, seat: {self.seat})>")
+                f" (row: {self.row}, seat: {self.seat})")
 
     def clean(self) -> None:
         if not (1 <= self.row <= self.movie_session.cinema_hall.rows):
